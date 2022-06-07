4daa6e63a0f647eb9b6b47955a513a9291cdab72
Vinicius <vteixeira212@gmail.com>
2022-06-07 20:48:57 -0300

    feat: calculando acuracia usando uma base de navegacao de usuarios

--- tracking/main.py
+++ tracking/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
 
 dados = pd.read_csv('tracking.csv')
 
@@ -13,3 +15,18 @@ dados = dados.rename(columns = mapa)
 
 x = dados[["principal","como_funciona","contato"]]
 y = dados["comprou"]
+
+treino_x = x[:75]
+treino_y = y[:75]
+
+teste_x = x[75:]
+teste_y = y[75:]
+
+model = LinearSVC()
+model.fit(treino_x, treino_y)
+
+previsoes = model.predict(teste_x)
+
+accuracy = accuracy_score(teste_y,previsoes) * 100
+
+print("acuraria de %.2f%%" % accuracy)
